2e045a15f379bf4f9b5d180a29e9e3ebc09006f8
harrison-spangler <harrison.spangler@gmail.com>
2026-01-30 10:35:49 -0500

    Added equivalence classes.

--- src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
@@ -24,21 +24,22 @@ class BankAccountTest {
 
     @Test
     void isEmailValidTest(){
+        // Equivalence class of testing on overall valid email addresses that are expected/normal
         assertTrue(BankAccount.isEmailValid( "a@b.com")); // valid email address
-
+        // Equivalence class of testing on local part that are valid
         assertTrue(BankAccount.isEmailValid("abc-d@mail.com")); // valid email, not border
         assertTrue(BankAccount.isEmailValid("abc.def@mail.com")); //  valid email where local part contains -, not border
         assertTrue(BankAccount.isEmailValid("abc.def@mail.com")); // same as above
         assertTrue(BankAccount.isEmailValid("abc_def@mail.com")); // valid email where local part contains _, not border
-
+        // Equivalence class of testing on domain part that are valid
         assertTrue(BankAccount.isEmailValid("abc.def@mail.cc")); // valid email with a two letter domain, yes border case
         assertTrue(BankAccount.isEmailValid("abc.def@mail-archive.com")); // valid email with - in domain, not border
         assertTrue(BankAccount.isEmailValid("abc.def@mail.org")); // valid email with .org domain, not border
         assertTrue(BankAccount.isEmailValid("abc.def@mail.com")); // valid email with .com domain, not border
 
-
+        // Equivalence class of testing on overall structure of where the domain, local, and TLD are placed, and the @/.
         assertFalse(BankAccount.isEmailValid("")); // empty string
-
+        
         assertFalse(BankAccount.isEmailValid("test")); // invalid missing @ and domain, yes border
         assertFalse(BankAccount.isEmailValid("test@em@ail.com")); // invalid multiple @ symbols, yes border
         assertFalse(BankAccount.isEmailValid("test.email@com")); // invalid missing domain dot, yes border
@@ -47,13 +48,13 @@ class BankAccountTest {
         assertFalse(BankAccount.isEmailValid("test@email.")); // invalid missing .com name, yes border
         assertFalse(BankAccount.isEmailValid("test@.com")); // invalid missing domain name, yes border
 
-
+        // Equivalence class of testing on local part that are invalid
         assertFalse(BankAccount.isEmailValid("abc-@mail.com")); // local part ends with -, yes border
         assertFalse(BankAccount.isEmailValid("abc..def@mail.com")); // local part has consecutive dots, yes border
         assertFalse(BankAccount.isEmailValid(".abc@mail.com")); // local part starts with ., yes border
         assertFalse(BankAccount.isEmailValid("abc#def@mail.com")); // local part has invalid character #, yes border
 
-
+        // Equivalence class of testing on domain part that are invalid
         assertFalse(BankAccount.isEmailValid("abc.def@mail.c")); // domain part has only one letter after dot, yes border
         assertFalse(BankAccount.isEmailValid("abc.def@mail#archive.com")); // domain part has invalid character  of #, yes border
         assertFalse(BankAccount.isEmailValid("abc.def@mail")); // domain part missing dot, yes border
